b1ba1b70dfe7f7e0d13f1ddc543155bea3d55df4
Pascal Hartig <phartig@rdrei.net>
2016-04-25 18:55:48 +0100

    Reverse proxy some stuff

--- src/Lib/Server.hs
+++ src/Lib/Server.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell   #-}
 
@@ -9,11 +10,18 @@ module Lib.Server
     , port
     ) where
 
-
-import qualified Web.Scotty   as S
-
-import           Control.Lens
-import           Data.Default (Default (), def)
+import           Control.Lens                         ((^.), (&), makeLenses)
+import qualified Data.ByteString                      as BS
+import qualified Data.ByteString.Char8                as B8
+import           Data.CaseInsensitive                 (CI)
+import           Data.Default                         (Default (), def)
+import qualified Network.HTTP.Conduit                 as Conduit
+import           Network.HTTP.ReverseProxy            (ProxyDest (ProxyDest),
+                                                       WaiProxyResponse (..),
+                                                       waiProxyToSettings)
+import qualified Network.Wai                          as Wai
+import           Network.Wai.Handler.Warp             (runEnv)
+import           Network.Wai.Middleware.RequestLogger (logStdout)
 
 newtype Port = Port Int
   deriving (Read, Show)
@@ -32,9 +40,27 @@ makeLenses ''ServerOptions
 instance Default Port where
   def = Port 3000
 
+hostHeader :: BS.ByteString -> (CI BS.ByteString, BS.ByteString)
+hostHeader dest = ("Host", dest)
+
+setHostHeaderToDestination :: BS.ByteString -> Wai.Request -> Wai.Request
+setHostHeaderToDestination dest req =
+  req { Wai.requestHeaders = replaceHostHeader (Wai.requestHeaders req) }
+  where replaceHostHeader =
+          map (\header@(name, _) ->
+            case name of
+              "Host" -> hostHeader dest
+              _      -> header)
+
 server :: ServerOptions -> IO ()
-server opts =
-  S.scotty (unPort $ opts ^. port) $
-    S.get "/live/feed.m3u8" $ do
-      S.setHeader "Content-Type" "application/x-mpegURL"
-      S.text "Hello, World!"
+server opts = do
+  manager <- Conduit.newManager Conduit.tlsManagerSettings
+  runEnv (opts ^. port & unPort) (logStdout $ proxy opts manager)
+
+proxy :: ServerOptions -> Conduit.Manager -> Wai.Application
+proxy opts = waiProxyToSettings transform def
+  where
+    destBS =
+      opts ^. url & B8.pack
+    transform req =
+      return $ WPRModifiedRequestSecure (setHostHeaderToDestination destBS req) (ProxyDest destBS 443)
